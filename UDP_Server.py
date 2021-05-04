cb6bb929c2b977347c4a9de25edabe7fb3afafc3
RiichD <47849926+RiichD@users.noreply.github.com>
2021-05-04 12:27:20 +0200

    DHCP Offer (wireshark OK)

--- UDP_Server.py
+++ UDP_Server.py
@@ -1,13 +1,14 @@
 import socket as s
 import time as t
 import netifaces as ni
+import struct
 
 serverIP = ""
 serverPort = 67
 
 server = s.socket(s.AF_INET, s.SOCK_DGRAM)
-#server.setsockopt(s.SOL_SOCKET, s.SO_REUSEPORT, 1)
-#server.setsockopt(s.SOL_SOCKET, s.SO_BROADCAST, 1) # Broadcast
+server.setsockopt(s.SOL_SOCKET, s.SO_REUSEPORT, 1)
+server.setsockopt(s.SOL_SOCKET, s.SO_BROADCAST, 1) # Broadcast
 server.bind((serverIP, serverPort))
 print("server (" +  serverIP + "," + str(serverPort) + ") ready")
 
@@ -68,22 +69,40 @@ def dhcp_offer(data, addr):
 	value[dhcp_format[2]['field']] = b'\x06' #hlen
 	value[dhcp_format[3]['field']] = b'\x00' #hops
 	value[dhcp_format[4]['field']] = b'\x01\x05\x08\x09' #xid
-	value[dhcp_format[5]['field']] = b'\x01' #secs
+	value[dhcp_format[5]['field']] = b'\x00\x00' #secs
 	value[dhcp_format[6]['field']] = b'\x00\x00' #flags
 	value[dhcp_format[7]['field']] = s.inet_aton('192.168.1.50') #ciaddr
 	value[dhcp_format[8]['field']] = s.inet_aton(your_ip) #yiaddr
 	value[dhcp_format[9]['field']] = s.inet_aton(your_ip) #siaddr
 	value[dhcp_format[10]['field']] = data[10]['giaddr'] #giaddr
 	value[dhcp_format[11]['field']] = data[11]['chaddr'] #chaddr
+	value[dhcp_format[12]['field']] = bytearray(64) #sname
+	value[dhcp_format[13]['field']] = bytearray(128) #file
 	
+	magic_cookie = bytes([0x63, 0x82, 0x53, 0x63])
+	DHCPOptions1 = bytes([53 , 1 , 2]) # => option 53, length 1, DHCP Offer
+	"""
+	DHCPOptions2 = bytes([3, 4]) + s.inet_aton('255.255.255.0') #Subnet mask
+	DHCPOptions3 = bytes([3 , 4]) + s.inet_aton('192.168.1.1') #192.168.1.1 router
+	DHCPOptions4 = bytes([51 , 4 , 0x00, 0x01, 0x51, 0x80]) #86400s(1 day) IP address lease time
+	DHCPOptions5 = bytes([54 , 4 , 0xC0, 0xA8, 0x01, 0x01]) # DHCP server
+	"""
 	data_to_send = b""
-	
+
 	for val in value.values():
-		#print(val)
 		data_to_send += val
 	
+	data_to_send += magic_cookie
+	data_to_send += DHCPOptions1
+	#data_to_send += DHCPOptions2
+	#data_to_send += DHCPOptions3
+	#data_to_send += DHCPOptions4
+	#data_to_send += DHCPOptions5
+	data_to_send += bytes([255])
+	
 	print(f"Data to send:{data_to_send}\n")
-	print(f"Data send to:{addr}\n")
+	print(f"Data send to:{addr}")
+	server.sendto(data_to_send, ('255.255.255.255', 68))
 	server.sendto(data_to_send, addr)
 	print("Data sent!\n")
 	
