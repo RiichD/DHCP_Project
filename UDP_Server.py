eea9c0ba5b9f57c5e8b8c551a53e1978d8425a34
RiichD <47849926+RiichD@users.noreply.github.com>
2021-05-04 11:18:14 +0200

    DHCP Offer

--- UDP_Server.py
+++ UDP_Server.py
@@ -1,5 +1,6 @@
 import socket as s
 import time as t
+import netifaces as ni
 
 serverIP = ""
 serverPort = 67
@@ -9,6 +10,10 @@ server = s.socket(s.AF_INET, s.SOCK_DGRAM)
 #server.setsockopt(s.SOL_SOCKET, s.SO_BROADCAST, 1) # Broadcast
 server.bind((serverIP, serverPort))
 print("server (" +  serverIP + "," + str(serverPort) + ") ready")
+
+ni.ifaddresses('enp0s3')
+ip = ni.ifaddresses('enp0s3')[ni.AF_INET][0]['addr']
+print(f"Own IP: {ip}\n")
 
 """
 DHCP Format length in byte.
@@ -42,22 +47,53 @@ def data_decoder(data):
 	decoded_data = []
 	
 	for dhcp_f in dhcp_format:
-		if dhcp_f['field'] == "options": #Ignore options
-			break
 		value = {}
-		if dhcp_f['type'] == int:
-			value[dhcp_f['field']] = int.from_bytes(data[b_pos:b_pos+dhcp_f['length']], "big")
-		else:
-			value[dhcp_f['field']] = data[b_pos:b_pos+dhcp_f['length']].hex()
+		if dhcp_f['field'] == "options": #Ignore options
+			value[dhcp_f['field']] = data[b_pos:]
+			decoded_data.append(value)
+			break
+		value[dhcp_f['field']] = data[b_pos:b_pos+dhcp_f['length']]
 		decoded_data.append(value)
 		b_pos+=dhcp_f['length']
 	return decoded_data
-	
 
+def dhcp_offer(data, addr):
+	"""
+	Function to send response.
+	"""
+	
+	value = {}
+	value[dhcp_format[0]['field']] = b'\x02' #op_code
+	value[dhcp_format[1]['field']] = b'\x01' #htype
+	value[dhcp_format[2]['field']] = b'\x06' #hlen
+	value[dhcp_format[3]['field']] = b'\x00' #hops
+	value[dhcp_format[4]['field']] = b'\x01\x05\x08\x09' #xid
+	value[dhcp_format[5]['field']] = b'\x01' #secs
+	value[dhcp_format[6]['field']] = b'\x00\x00' #flags
+	value[dhcp_format[7]['field']] = s.inet_aton('192.168.1.50') #ciaddr
+	value[dhcp_format[8]['field']] = s.inet_aton('192.168.1.47') #yiaddr
+	value[dhcp_format[9]['field']] = s.inet_aton('192.168.1.47') #siaddr
+	value[dhcp_format[10]['field']] = data[10]['giaddr'] #giaddr
+	value[dhcp_format[11]['field']] = data[11]['chaddr'] #chaddr
+	
+	data_to_send = b""
+	
+	for val in value.values():
+		#print(val)
+		data_to_send += val
+	
+	print(f"Data to send:{data_to_send}\n")
+	print(f"Data send to:{addr}\n")
+	server.sendto(data_to_send, addr)
+	print("Data sent!\n")
+	
 def start():
 	while True:
-		data, addr = server.recvfrom(2048)
+		data, addr = server.recvfrom(2048) #DHCP DISCOVER
 		print(f"Server has received:\n{data}\n")
-		print(f"{data_decoder(data)}\n")
-
+		
+		decoded_data = data_decoder(data)
+		print(f"{decoded_data}\n")
+		
+		dhcp_offer(decoded_data, addr) #DHCP OFFER
 start()
